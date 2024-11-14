28fc28680f14a0ce9b536bddba5726a4017c2c94
AnnapuraniA <annuarun01@gmail.com>
2024-11-14 20:52:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-mobileMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
+const countdown = document.getElementById("countdown");
+setTimeout(function () {
+    countdown.innerText = "10";
+    setTimeout(function () {
+        countdown.innerText = "9";
+        setTimeout(function () {
+            countdown.innerText = "8";
+            setTimeout(function () {
+                countdown.innerText = "7";
+                setTimeout(function () {
+                    countdown.innerText = "6";
+                    setTimeout(function () {
+                        countdown.innerText = "5";
+                        setTimeout(function () {
+                            countdown.innerText = "4";
+                            setTimeout(function () {
+                                countdown.innerText = "3";
+                                setTimeout(function () {
+                                    countdown.innerText = "2";
+                                    setTimeout(function () {
+                                        countdown.innerText = "1";
+                                        setTimeout(function () {
+                                            countdown.innerText = "Happy Independence Day";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
